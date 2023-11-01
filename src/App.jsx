613a63bfdce23c5bd7ba6817657fd9631f63f7df
na0260 <samjr.008@gmail.com>
2023-11-02 01:09:30 +0600

    layout created

--- src/App.jsx
+++ src/App.jsx
@@ -17,20 +17,41 @@ function App() {
     ];
   return (
     <>
-        <div className="flex flex-col items-center">
-            <div className="max-w-4xl w-full">
-                <div className="mb-8">
-                    <img src={imageUrl+'/'+images[0]} alt="Main" className="w-full"/>
+        <div className="container mx-auto border-2 rounded-2xl">
+            <div className="grid grid-cols-2 p-5 border-b-2">
+                <div>
+                    <input  type="checkbox"/>
+                    <label className="ps-2">Image Selected</label>
                 </div>
-                <div className="grid grid-cols-3 gap-4">
-                    {images.map((image, index) => (
-                        <img
-                            key={index}
-                            src={imageUrl+'/'+images[index]}
-                            alt={`Thumbnail ${index}`}
-                            className="w-full cursor-pointer"
-                        />
-                    ))}
+                <div>
+                    <a className="text-decoration-none float-right text-red-500" href="#">Delete Images</a>
+                </div>
+            </div>
+            <div className="p-5">
+                <div className="grid grid-cols-5 gap-5">
+                    <div className="col-span-2 row-span-2 border-2 rounded-2xl overflow-hidden relative">
+                        <img src={`${imageUrl}/${images[0]}`} alt="image-1" />
+                        <label htmlFor="imageCheckbox" className="absolute inset-0 flex opacity-0 hover:opacity-100 transition-opacity duration-300 bg-black bg-opacity-50">
+                            <input type="checkbox" id="imageCheckbox" className="hidden" />
+                            <span className="text-white text-2xl absolute top-5 left-5">✓</span>
+                        </label>
+                    </div>
+                    {images.map((image, index) => {
+                        if (index > 0 && index < 15) {
+                            return (
+                                <div className="border-2 rounded-2xl overflow-hidden relative" key={index}>
+                                    <img src={`${imageUrl}/${image}`} alt={`image-${index}`} />
+                                    <label htmlFor="imageCheckbox" className="absolute inset-0 flex opacity-0 hover:opacity-100 transition-opacity duration-300 bg-black bg-opacity-50">
+                                        <input type="checkbox" id="imageCheckbox" className="hidden" />
+                                        <span className="text-white text-2xl absolute top-5 left-5">✓</span>
+                                    </label>
+                                </div>
+                            )
+                        }
+                    })}
+                    <div className="border-dotted border-2 rounded-2xl overflow-hidden">
+                        <img src="/src/assets/images/add-image.png" alt="add" />
+                    </div>
                 </div>
             </div>
         </div>
