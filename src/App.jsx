ae5d498f46e710d6022817df77f6792eb6785739
na0260 <samjr.008@gmail.com>
2023-11-03 00:36:01 +0600

    add image working

--- src/App.jsx
+++ src/App.jsx
@@ -1,20 +1,19 @@
 import './App.css'
+import {useState} from "react";
 
 function App() {
+    const [selectedImages, setSelectedImages] = useState([]);
+    const selectFile = (e)=>{
+        const selectedFiles = e.target.files;
+        const selectedFilesArr = Array.from(selectedFiles);
+        const imgArr = selectedFilesArr.map((file) => {
+            return URL.createObjectURL(file);
+        });
+
+        setSelectedImages((prevImages) => prevImages.concat(imgArr));
+        e.target.value = "";
+    }
     const imageUrl = '/src/assets/images'
-    const images = [
-        'image-1.webp',
-        'image-2.webp',
-        'image-3.webp',
-        'image-4.webp',
-        'image-5.webp',
-        'image-6.webp',
-        'image-7.webp',
-        'image-8.webp',
-        'image-9.webp',
-        'image-10.jpeg',
-        'image-11.jpeg',
-    ];
   return (
     <>
         <div className="container mx-auto border-2 rounded-2xl">
@@ -29,18 +28,22 @@ function App() {
             </div>
             <div className="p-5">
                 <div className="grid grid-cols-5 gap-5">
-                    <div className="col-span-2 row-span-2 border-2 rounded-2xl overflow-hidden relative">
-                        <img src={`${imageUrl}/${images[0]}`} alt="image-1" />
-                        <label htmlFor="imageCheckbox" className="absolute inset-0 flex opacity-0 hover:opacity-100 transition-opacity duration-300 bg-black bg-opacity-50">
-                            <input type="checkbox" id="imageCheckbox" className="hidden" />
-                            <span className="text-white text-2xl absolute top-5 left-5">✓</span>
-                        </label>
-                    </div>
-                    {images.map((image, index) => {
-                        if (index > 0 && index < 15) {
+                    {selectedImages.map((image, index) => {
+                        if (index === 0) {
+                            return (
+                                <div key={index} className="col-span-2 row-span-2 border-2 rounded-2xl overflow-hidden relative">
+                                    <img src={image} alt="image-1"/>
+                                    <label htmlFor="imageCheckbox"
+                                           className="absolute inset-0 flex opacity-0 hover:opacity-100 transition-opacity duration-300 bg-black bg-opacity-50">
+                                        <input type="checkbox" id="imageCheckbox" className="hidden"/>
+                                        <span className="text-white text-2xl absolute top-5 left-5">✓</span>
+                                    </label>
+                                </div>
+                            )
+                        } else {
                             return (
                                 <div className="border-2 rounded-2xl overflow-hidden relative" key={index}>
-                                    <img src={`${imageUrl}/${image}`} alt={`image-${index}`} />
+                                    <img src={image} alt={`image-${index}`} />
                                     <label htmlFor="imageCheckbox" className="absolute inset-0 flex opacity-0 hover:opacity-100 transition-opacity duration-300 bg-black bg-opacity-50">
                                         <input type="checkbox" id="imageCheckbox" className="hidden" />
                                         <span className="text-white text-2xl absolute top-5 left-5">✓</span>
@@ -50,7 +53,11 @@ function App() {
                         }
                     })}
                     <div className="border-dotted border-2 rounded-2xl overflow-hidden">
-                        <img src="/src/assets/images/add-image.png" alt="add" />
+                        <label htmlFor="upload" className="cursor-pointer">
+                            <img src="/src/assets/images/add-image.png" alt="add" />
+                        <input type="file" id="upload" className="hidden " multiple
+                               accept="image/png , image/jpeg, image/webp" onChange={selectFile}/>
+                        </label>
                     </div>
                 </div>
             </div>
